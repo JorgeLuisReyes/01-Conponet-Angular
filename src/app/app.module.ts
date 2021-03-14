a16e3bc9beddda89b050698e676f69ac70388834
Jorge Reyes <reyesjorge48@gmail.com>
2021-03-14 15:49:27 +0100

    Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContastComponent } from './pages/contast/contast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContastComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/contast/contast.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contast',
+  templateUrl: './contast.component.html',
+})
+export class ContastComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
